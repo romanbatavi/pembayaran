0b0f7dcf31d461f6c9374c701cbe105e831de298
romanbatavi <romanbatavi98@gmail.com>
2021-03-16 19:24:44 +0700

    remove wm footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,11 +1,3 @@
-<!-- Main Footer -->
-<footer class="main-footer">
-    <strong>Copyright &copy; 2020 <a href="http://adminlte.io">SMK PGRI PAKISAJI</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 1.0.0
-    </div>
-</footer>
 </div>
 <!-- ./wrapper -->
 
